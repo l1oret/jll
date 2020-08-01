14c1df8ee3efa4f339b4d0e6113eedd98ade462b
l1oret <lloret.joseluis@gmail.com>
2020-08-01 23:12:43 +0200

    Remove unused plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -56,18 +56,12 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
-      },
-    },
     `gatsby-plugin-feed`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `Out of the loop — Un blog de José Luis Lloret`,
+        short_name: `Out of the loop`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
@@ -81,9 +75,6 @@ module.exports = {
       options: {
         pathToConfigModule: `src/utils/typography`,
       },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    }
   ],
 }
